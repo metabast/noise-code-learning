4688d7471b021fe5104a2d20f42771dca1d12c1b
Sébastien Vincent <metabast@gmail.com>
2021-11-08 23:49:10 +0100

    island

--- src/terrains/Terrain1.js
+++ src/terrains/Terrain1.js
@@ -6,6 +6,12 @@ const lerp = window.lerp = (x, y, t) => x * (1-t) + y * t;
 const clamp = window.clamp = (x, min=0, max=1) => Math.min(max, Math.max(min, x));
 const invLerp = window.invLerp = (x, y, t) => clamp( (t - x) / (y - x) );
 const range = window.range = (x1, y1, x2, y2, t) => lerp(x2, y2, invLerp(x1, y1, t));
+const pow = Math.pow;
+const max = Math.max;
+const min = Math.min;
+const PI = Math.PI;
+const abs = Math.abs;
+const cos = Math.cos;
 
 
 import FastNoiseLite from "../vendors/FastNoiseLite.js";
@@ -23,10 +29,10 @@ noise2.SetFrequency(1.2);
 
 let noise3 = new FastNoiseLite(10);
 noise3.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
-noise3.SetFrequency(1.0);
+noise3.SetFrequency(0.6);
 noise3.SetFractalType(FastNoiseLite.FractalType.FBm);
 noise3.SetFractalOctaves(7.0);// default 3
-noise3.SetFractalLacunarity(2.5);// default 2
+noise3.SetFractalLacunarity(1.9);// default 2
 noise3.SetFractalGain(0.5);// default .5
 
 
@@ -46,28 +52,39 @@ plane.add(wireframe);
 plane.rotateX(-90*(Math.PI/180));
 const color = new THREE.Color();
 const colors = geometry.attributes.color;
-console.log(color)
+
+const vr = new THREE.Vector2(1,1); 
+const vrl = vr.length(); 
 
 for(let i=0; i< geometry.attributes.position.array.length; i+=3){
-    let vec = new THREE.Vector2( geometry.attributes.position.array[i], geometry.attributes.position.array[i+1]);
-    let noiseVal = noise2.GetNoise(vec.x,vec.y);
-    let ly = range(-1, 1, 0, 1, noiseVal);
-    noiseVal = lerp(clamp(noise.GetNoise(vec.x,vec.y), -1, 1), clamp(noise3.GetNoise(vec.x,vec.y)*0.10, -1, 1), ly)
+    let p = new THREE.Vector2( geometry.attributes.position.array[i], geometry.attributes.position.array[i+1]);
+    let pl = p.length();
+    let pln = pl * (1/vrl);
+
+    // radial gradient
+    let rg = pow( min( cos(PI * pln / 2.0), 1.0 - abs(pln)), 4.0 );
+    // rg = rg * 2 - 1;
+
+    let noiseVal = rg;
+
+    let ly = range(0, 1, 0, 2, rg);
+    noiseVal = lerp( 0, noise3.GetNoise(p.x,p.y)-.1, ly)
 
     // if(noiseVal >= 0)
     //     noiseVal = clamp(noise.GetNoise(vec.x,vec.y), -1, 1);
     // else
     //     noiseVal *= 1-clamp(noise3.GetNoise(vec.x,vec.y), -1, 1);
 
-    let posZ = clamp(noiseVal, -1, 1);
-    geometry.attributes.position.array[i+2] = posZ*0.2;
+    // let posZ = clamp(noiseVal, -1, 1);
+    let posZ = noiseVal;
+    geometry.attributes.position.array[i+2] = posZ;
 
     // let colorZ = clamp(noiseVal, -1, 1);
-    let colorZ = range(0, 1, 0, 1, noiseVal);
+    let colorZ = range(0, 1, 0, 1.0, posZ);
 
-    if(colorZ <= 0)
+    if(colorZ <= 0.005)
         colors.setXYZ((i/3), 0.05,0.2,0.3);
-    else if(colorZ <=.05){
+    else if(colorZ <=0.05){
         color.setHex(0xD7D7A5);
         colors.setXYZ((i/3), color.r, color.g, color.b);
     }
